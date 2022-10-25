fcc4c4d30efa13c0df16cf0547cd916a991f0b58
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-26 00:23:39 +0600

    carousel  added

--- src/Pages/Home/Carousels/Carousels.js
+++ src/Pages/Home/Carousels/Carousels.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import './Carousels.css'
 
 const Carousels = () => {
     return (
@@ -7,37 +8,37 @@ const Carousels = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          src="Images/Carousel/1.png"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className='text_back_blurr'>
+          <h1>CHOOSE</h1>
+          <p>Choose the Course you want to learn</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          src="Images/Carousel/2.png"
           alt="Second slide"
         />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className='text_back_blurr'>
+          <h1>Learn</h1>
+          <p>Learn the course with heart</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src="Images/Carousel/3.png"
           alt="Third slide"
         />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
+        <Carousel.Caption className='text_back_blurr'>
+          <h1>Develop</h1>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Develop Your Skills.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
