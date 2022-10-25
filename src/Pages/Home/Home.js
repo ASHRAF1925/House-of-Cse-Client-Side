987d85a7f124980b647146a1a7e571d0e7272dde
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-26 00:11:38 +0600

    basic router  added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Shared/Header/Header';
+import Carousels from './Carousels/Carousels';
+
+
+const Home = () => {
+    return (
+        <div>
+          <Header></Header>
+          <Carousels></Carousels>
+        </div>
+    );
+};
+
+export default Home;
