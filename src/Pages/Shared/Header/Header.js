033d510225ecbeb4ef5c292ef8f2e0e48aef9d4f
ASHRAF1925 <1925ashraf@gmail.com>
2022-10-27 03:48:34 +0600

    theme done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,96 +1,145 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 import logo from "../../../Assets/Images/logo-removebg-preview.png";
-import "./Header.css"
-import { useContext } from 'react';
-import { AuthContext } from '../../../Contexts/UserContext';
+import "./Header.css";
+import { useContext } from "react";
+import { AuthContext } from "../../../Contexts/UserContext";
+import { useState } from "react";
+import { useEffect } from "react";
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
 
-  const {user,logout}=useContext(AuthContext);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
 
-  const handlelogout=()=>{
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
+
+  const handlelogout = () => {
     logout()
-    .then(() => { 
-      // Sign-out successful.
-      console.log("signout successful")
-    }).catch((error) => {
-     console.log(error);
-    });
-  }
-    return (
-        <div>
-        <Navbar expand="lg" id='backgroundColor'>
-      <div className="d-flex  me-auto container ">
-        <div>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="100"
-              height="100"
-              className="d-inline-block align-top"
-              alt=""
-            />
+      .then(() => {
+        // Sign-out successful.
+        console.log("signout successful");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <Navbar expand="lg" id="backgroundColor">
+        <div className="d-flex  me-auto container ">
+          <div>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                width="100"
+                height="100"
+                className="d-inline-block align-top"
+                alt=""
+              />
+            </Navbar.Brand>
+          </div>
+          <Navbar.Brand href="/" className="title">
+            House Of CSE
           </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav " />
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav
+              variant="pills"
+              defaultActiveKey="/"
+              className="ms-auto my-auto align-items-center "
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="/">
+                  <Link to="/" className="text-decoration-none  text_color">
+                    {" "}
+                    Home
+                  </Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="/Courses">
+                  <Link
+                    to="/Courses"
+                    className="text-decoration-none  text_color"
+                  >
+                    {" "}
+                    Courses
+                  </Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="/Blogs">
+                  <Link
+                    to="/Blogs"
+                    className="text-decoration-none  text_color"
+                  >
+                    {" "}
+                    Blogs
+                  </Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="/FAQS">
+                  <Link to="/FAQS" className="text-decoration-none  text_color">
+                    {" "}
+                    FAQS
+                  </Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Form>
+              
+                <label class="switch mx-3">
+                  <input type="checkbox" id="togBtn" />
+                  <div onClick={toggleTheme} class="slider round">
+                    <span class="on">Light</span>
+                    <span class="off">Dark</span>
+                  </div>
+                </label>
+              </Form>
+
+              {user?.uid && (
+                <div>
+                  {" "}
+                  <img
+                    src="Images/logo-removebg-preview.png"
+                    width="100"
+                    height="100"
+                    className="d-inline-block align-top rounded-circle"
+                    alt=""
+                  />
+                  <span>welcome {user.email}</span>
+                </div>
+              )}
+              {user?.uid ? (
+                <Link>
+                  <button onClick={handlelogout}>Sign Out</button>
+                </Link>
+              ) : (
+                <Link to="/login">
+                  <button className="btn btn-primary">Login</button>
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
         </div>
-        <Navbar.Brand href="/"  className="title">
-         House Of CSE
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav " />
-        <Navbar.Collapse id="basic-navbar-nav ">
-          <Nav variant="pills" defaultActiveKey="/" className="ms-auto my-auto align-items-center ">
-          <Nav.Item>
-              <Nav.Link eventKey="/"><Link to='/' className="text-decoration-none  text_color"> Home</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/Courses"><Link to='/Courses' className="text-decoration-none  text_color"> Courses</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/Blogs"><Link to='/Blogs' className="text-decoration-none  text_color"> Blogs</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="/FAQS"><Link to='/FAQS' className="text-decoration-none  text_color"> FAQS</Link></Nav.Link>
-            </Nav.Item>
-            <Form>
-      <Form.Check 
-        type="switch"
-        id="custom-switch"
-        label="Theme"
-      />
-      
-    </Form>
-   
-             {user?.uid && <div>  <img 
-              src="Images/logo-removebg-preview.png"
-              width="100"
-              height="100"
-              className="d-inline-block align-top rounded-circle"
-              alt=""
-            /><span>welcome {user.email}</span></div> }
-            {
-              user?.uid?
-              <Link><button onClick={handlelogout}>Sign Out</button></Link>
-              :
-              <Link to="/login"><button className='btn btn-primary' >Login</button></Link>
-
-
-            }
-
-          
-            
-            
-            
-            
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-        </div>
-    );
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
